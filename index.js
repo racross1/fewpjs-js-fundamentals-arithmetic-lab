feec8437413e1b657c3d3fbef0a3f28edbf325a8
racross1 <racross1@gmail.com>
2021-01-21 13:12:13 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 1
+let num2 = 62
+
+let multiply = num1 * num2
+
+
+let random = Math.floor(Math.random()) + 0.1
+
+let mod = 24 % 5
+
+let max = Math.max(1, 2, 3, 20)
+
